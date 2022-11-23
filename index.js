a8f252f72aa766e27655fdcb3cd6547a9d77cdfa
spectr-e <devs.josia@gmail.com>
2022-11-23 13:32:06 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob";
+var bestCustomer;
+function upperCaseCustomerName() {
+  return (customerName = customerName.toUpperCase());
+}
+function setBestCustomer() {
+  return (bestCustomer = "not bob");
+}
+function overwriteBestCustomer(name) {
+  return (bestCustomer = name);
+}
+const leastFavoriteCustomer = "nyanyez";
+
+function changeLeastFavoriteCustomer() {
+  return (leastFavoriteCustomer = "ex");
+}
